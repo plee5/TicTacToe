c89c8499189345a70159fd324c3164505dbda9b5
Patrick Lee <p.lee@berkeley.edu>
2017-05-19 21:54:56 -0700

    Removed unnecessary methods

--- TicTacToePlayer.java
+++ TicTacToePlayer.java
@@ -7,7 +7,7 @@ Simple one game of TicTacToe.
 @date March 26, 2017
 */
 
-public class TicTacToePlayer() {
+public class TicTacToePlayer {
     
     public char piece;
     public TicTacToePlayer(char x) {
@@ -17,17 +17,17 @@ public class TicTacToePlayer() {
     /** Get the player for this player. */
     public int[] play() {
         ///how do i want to do this
-        Scanner scan = new Scanner();
+        Scanner scan = new Scanner(System.in);
         int toRet[] = new int[2];
         int row = -1;
         int col = -1;
         if (scan.hasNext()) {
-            System.out.println("Please select your row.");
-            row = scan.next();
+            System.out.println("Please select your row:");
+            row = Integer.parseInt(scan.next());  //currently assumes you enter int
             toRet[0] = row;
             if (scan.hasNext()) {
-                System.out.println("Please select your column.");
-                col = scan.next();
+                System.out.println("Please select your column:");
+                col = Integer.parseInt(scan.next());
                 toRet[1] = col;
             } else {
                 throw new IllegalArgumentException();
@@ -36,24 +36,9 @@ public class TicTacToePlayer() {
         return toRet;
     }
 
+    public char getPiece() {
+        return piece;
+    }
 
-    /** Returns true if the board isn't full meaning the player can play. */
-    public boolean canPlay() {
-        return !isBoardFull();
-    }
-    
-    // Loop through all cells of the board and if one is found to be empty (contains char '-') then return false.
-    // Otherwise the board is full.
-    public boolean isBoardFull() {
-        boolean isFull = true;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
-                    isFull = false;
-                }
-            }
-        }
-        return isFull;
-    }
 
 }
